5f68665621bafe341f74aabaf7d24401c4b9f5f4
June Choe <jchoe001@gmail.com>
2020-11-16 21:44:09 -0500

    color palettes

---
+++ R/colors.R
@@ -0,0 +1,9 @@
+#' Complete list of palettes
+#'
+#' @export
+junebug_palettes <- list(
+  CranberryWhiteGrub = c("#591E29", "#794284", "#C8787A", "#373F40", "#A7BFB4"),
+  CubanMay = c("#D9C091", "#8C7251", "#8C3211", "#591202", "#291F16"),
+  Mican = c("#7C1C0E", "#98836E", "#32201E", "#B17052", "#D14D36"),
+  NorthernJune = c("#26141B", "#BF6734", "#8C2727", "#A66D6D", "#34160A")
+)
