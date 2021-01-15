bd4084512bd308c0e6e4e3af5d0552449aba7b0b
June Choe <jchoe001@gmail.com>
2021-01-15 18:03:29 +0900

    rename unit_label to one_label

--- R/ggplot2_helpers.R
+++ R/ggplot2_helpers.R
@@ -1,9 +1,10 @@
-#' Annotate a single unit text with a unit label
+#' Annotate a single break on an axis with a label
 #'
 #' For use in the `labels` argument of `ggplot2::scale_*_continuous`
 #'
-#' @param x Vector of labels
-#' @param unit_label A string attached at the end of the label
+#' @param x Vector of axis breaks
+#' @param suffix A string attached to the end of the text. Defaults to "ms".
+#' @param prefix A string attached to the beginning of the text. Defaults to "".
 #' @param position Which label the annotation should go to. Either 'first' (bottom/left) or 'last' (top/right).
 #'
 #' @export
@@ -11,19 +12,19 @@
 #' @examples
 #' \dontrun{
 #' my_plot <- ggplot(mtcars, aes(mpg, hp)) + geom_point()
-#' my_plot + scale_y_continuous(labels = label_unit)
-#' my_plot + scale_y_continuous(labels = function(x) label_unit(x, "%"))
-#' my_plot + scale_y_continuous(labels = function(x) label_unit(x, "%", "first"))
-#' my_plot + scale_y_continuous(labels = function(x) label_unit(x, "sec"))
+#' my_plot + scale_y_continuous(labels = one_label)
+#' my_plot + scale_y_continuous(labels = function(x) one_label(x, "%"))
+#' my_plot + scale_y_continuous(labels = function(x) one_label(x, "%", position = "first"))
+#' my_plot + scale_y_continuous(labels = function(x) one_label(x, prefix = "$"))
 #' }
-label_unit <- function(x, unit_label = "ms", position = "last") {
+one_label <- function(x, suffix = "ms", prefix = "",  position = "last") {
   labs <- which(!is.na(x))
   if (position == "last") {
     last_idx <- labs[length(labs)]
-    replace(x, last_idx, paste0(x[last_idx], unit_label))
+    replace(x, last_idx, paste0(prefix, x[last_idx], suffix))
   } else if (position == "first") {
     first_lab <- labs[1]
-    replace(x, first_lab, paste0(x[first_lab], unit_label))
+    replace(x, first_lab, paste0(prefix, x[first_lab], suffix))
   } else {
     stop("Value for argument `position` must 'first' or 'last'")
   }
