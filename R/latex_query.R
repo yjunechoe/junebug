a5ff5108fef1164de2a758922868c8c61b663dbe
June Choe <jchoe001@gmail.com>
2021-01-17 15:48:59 +0900

    add latex_query

---
+++ R/latex_query.R
@@ -0,0 +1,28 @@
+#' Generate a CodeCogs URL that renders latex as image
+#'
+#' Interactive version at https://www.codecogs.com/latex/eqneditor.php
+#'
+#' @param formula An appropriately escaped string of LaTex formula. Using the new string literal syntax `r"(...)"` from R 4.0.0 on the existing formula is recommended.
+#' @param size Font size. One of "tiny", "small", "normal", "large", or "huge"
+#' @param dpi Image resolution. Defaults to 300
+#' @param inline Render as inline formula? If TRUE, results in a more compact image.
+#'
+#' @return A character string of the URL
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' latex_query(r"(\beta + \alpha)")
+#' latex_query("\\beta + \\alpha")
+#' magick::image_read(latex_query(r"(\beta + \alpha)"))
+#' }
+latex_query <- function(formula, size = "normal", dpi = 300, inline = FALSE) {
+  paste0(
+    "https://latex.codecogs.com/png.latex?",
+    if (inline) "%5Cinline%20",
+    "%5Cdpi%7B", dpi, "%7D%20",
+    "%5C", if (size %in% c("tiny", "small", "normal", "large", "huge")) size, "%20",
+    utils::URLencode(formula, reserved = TRUE)
+  )
+}
