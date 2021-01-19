9cd8e3023b6c57ea8e914bc6798c99dfbac1e285
June Choe <jchoe001@gmail.com>
2021-01-20 08:00:13 +0900

    fixes to icon_html css format

--- R/miscellaneous.R
+++ R/miscellaneous.R
@@ -32,27 +32,19 @@ latex_query <- function(formula, size = "normal", dpi = 300, transparent = FALSE
 #' Find a Font Awesome icon in the appropriate html format
 #'
 #' @param icon A character string that matches one or more Font Awesome icon names
-#' @param type One of "brand", "regular", and "solid"
-#' @param unicode Should it return the unicode value for the icon wrapped in the `span` tag? If `FALSE`, returns the icon class wrapped in the `i` tag
+#' @param type "css" returns the icon wrapped in `span`; "class" returns the icon wrapped in `i`
 #'
-#' @return A character vector of matches in the specified format
+#' @return The first icon match in the specified format
 #' @export
-icon_html <- function(icon, type = "solid", unicode = TRUE) {
+icon_html <- function(icon, type = "css") {
   icon_table <- utils::read.csv("https://yjunechoe.netlify.app/data/fa_table.csv")
-  type <- switch(
-    type,
-    "brand" = "Brands Regular",
-    "regular" = "Free Regular",
-    "solid" = "Free Sollid",
-    stop("type must be one of c('brand', 'regular', 'solid')")
-  )
-  matched <- icon_table[(grepl(icon, icon_table$icon)),]
-  if (nrow(matched) > 0) {
-    if (unicode) {
-      stats::setNames(paste0("<span style='font-family: \"Font Awesome 5 ", type, "\"'>", matched$unicode , "</span>"),  matched$icon)
-    } else {
-      paste0("<i class='", matched$icon, "'</i>")
-    }
+  if (!type %in% c("css", "class")) stop("type must be one of c('css', 'class')")
+  matched <- icon_table[(grepl(icon, icon_table$name)),]
+  if (nrow(matched) > 1) {
+    matched[[type]][1]
+    message(paste("Returning first match. Other matches:", matched[[type]] - 1))
+  } else if (nrow(matched) == 1L) {
+    matched[[type]]
   } else {
     stop(paste("No match found for", icon, "in", "Font Awesome 5", type))
   }
